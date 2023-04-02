ab60851164ccb7ad502aed755b60bd285d7adc46
yefeng <1226325909@qq.com>
2023-04-02 22:01:40 +0800

    fate:update HttpContextUtils for getTokenByHeader

--- netdisk-common/src/main/java/com/yefeng/netdisk/common/util/HttpContextUtils.java
+++ netdisk-common/src/main/java/com/yefeng/netdisk/common/util/HttpContextUtils.java
@@ -7,7 +7,25 @@ import javax.servlet.http.HttpServletRequest;
 
 public class HttpContextUtils {
 
+	private static final String AUTHORIZATION_TOKEN = "token";
+
 	public static HttpServletRequest getHttpServletRequest() {
 		return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
 	}
+	public static String getTokenByHeader() {
+		HttpServletRequest request = getHttpServletRequest();
+		String rightToken = request.getHeader(AUTHORIZATION_TOKEN);
+		JWTUtil.validateToken(rightToken);
+		return rightToken;
+	}
+
+
+
+	public static String getHeader(String key) {
+		HttpServletRequest request = getHttpServletRequest();
+		String header = request.getHeader(key);
+		return header;
+	}
+
+
 }
